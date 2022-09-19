24d386014ebc179ac20b666cc18af47997a7c91f
K M Rifat ul alom <kmrifat@gmail.com>
2022-09-19 23:18:47 +0600

    update gitignore

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
@@ -6,6 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from contact.views import *
+from core import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -27,4 +27,4 @@ urlpatterns = [
     path('country-contacts/<str:country>/', CountryContactAPIView.as_view())
 ]
 
-urlpatterns + static(settings.STATIC_URL, settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
